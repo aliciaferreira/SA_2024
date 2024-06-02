3424b57be25cd8e8a78d45fc96f4344e175088a0
Alicia Ferreira <alicia@easytechgreen.com>
2024-06-02 18:42:48 -0300

    add preference: music

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$music=(isset($_GET['music'])?$_GET['music']:"No cargaste un tipo de música preferida");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Tu música preferida es: $music";
 
 ?>
