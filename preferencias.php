c5a45f9856fc34a68c69c532686681f1c43d4a84
MarFerGit <marinafernandarodriguez443@gmail.com>
2024-06-06 10:06:50 -0300

    add preference: letra

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$letra=(isset($_GET['letra'])?$_GET['letra']:"No cargaste tu Estilo de Letra preferido");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Tu estilo de letra es : $letra";
 
 ?>
