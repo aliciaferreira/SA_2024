0c444d22ae52fb71b6a70899212a80ea1eb444f2
Juan Lopez <david16115@outlook.com>
2024-06-06 21:41:12 -0300

    Agregue la preferencia juegoPreferido

--- preferencias.php
+++ preferencias.php
@@ -4,7 +4,11 @@ require("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
+$juegoPreferido=(isset($_GET['juegoPreferido'])?$_GET['juegoPreferido']:"No cargaste un color preferido");
+
+echo "Hola $usuario <br>";
+echo "Tu color preferido es: $color <br>";
+echo "Tu color preferido es: $juegoPreferido <br>";
+
 
 ?>
