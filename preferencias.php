cbfb7a01c10f6576fb43f3b8c7b8d0e3a60d98c2
J Camara <jcamara814@gmail.com>
2024-06-06 21:48:39 -0300

    add preferencia: serie de tv

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,11 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$serie=(isset($_GET['serie'])?$_GET['serie']:"No cargaste una serie de television preferida");
 
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
+
+echo "Hola $usuario <br>";
+echo "Tu color preferido es: $color <br>";
+echo "Tu serie preferida es: $serie <br>";
 
 ?>
