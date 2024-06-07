9253e6917d3e2086cc19f45730cf0ce5f8a8c752
Alicia Ferreira <alicia@easytechgreen.com>
2024-06-06 21:48:46 -0300

    add preference: flor

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$flor=(isset($_GET['flor'])?$_GET['flor']:"No cargaste una flor preferida");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Tu flor preferida es: $flor";
 
 ?>
