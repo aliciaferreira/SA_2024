6394c07b823d5493e8d7164f8b0dfbb08691d357
matias <matiaskarlen0678@gmail.com>
2024-06-06 21:49:43 -0300

    add preference weather

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
+$clima=(isset($_GET['clima'])?$_GET['clima']:"No cargaste un clima");
+?><br><br><?php
+echo "Hola $usuario";?><br><br><?php
+echo "Tu color preferido es: $color";?><br><br><?php
+echo "Tu clima preferido es: $clima";?><br><br><?php
 
 ?>
