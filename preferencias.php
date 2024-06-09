b536dc733e968a50da915bc54eafe35da8adbfe5
unknown <betianajuarez29@gmail.com>
2024-06-09 17:45:43 -0300

    Agregar preferencia nombre

--- preferencias.php
+++ preferencias.php
@@ -2,9 +2,10 @@
 require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
+$nombre=(isset($_GET['nombre'])?$_GET['nombre']:"No cargaste un nombre");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 
-echo "Hola $usuario";
+echo "Hola $nombre ($usuario)";
 echo "Tu color preferido es: $color";
 
 ?>
