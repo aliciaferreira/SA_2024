17813a06cd408e57e962f1104e88a9374dc3d1cc
FernandoALemos <fernando.alberto.lemos@gmail.com>
2024-06-13 14:14:26 -0300

    add reference: juego de mesa

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$boardgame=(isset($_GET['boardgame'])?$_GET['boardgame']:"No cargaste tu juego de mesa preferido");
 
-echo "Hola $usuario";
+echo "Hola $usuario!! <br>";
 echo "Tu color preferido es: $color";
+echo " y tu juego de mesa preferido es: $boardgame";
 
 ?>
