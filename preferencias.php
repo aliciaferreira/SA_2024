e1bf4281c41a99b3beb45da007066342f9d58fe0
binimz <bdvazquez29@gmail.com>
2024-06-14 00:09:45 -0300

    Add preference: Serie favorita

--- preferencias.php
+++ preferencias.php
@@ -6,11 +6,13 @@ $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $flor=(isset($_GET['flor'])?$_GET['flor']:"No cargaste una flor preferida");
 $music=(isset($_GET['music'])?$_GET['color']:"No cargaste un tipo de musica preferida");
 $boardgame=(isset($_GET['boardgame'])?$_GET['boardgame']:"No cargaste tu juego de mesa preferido");
+$serie=(isset($_GET['serie'])?$_GET['serie']:"No cargaste tu serie favorita");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu flor preferida es: $flor";
 echo "Tu musica preferida es: $music";
 echo "Tu juego de mesa preferido es: $boardgame";
+echo "Tu serie favorita es: $serie";
 
 ?>
