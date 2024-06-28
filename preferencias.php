99d3fdd08ae64dee4e86b9811e3bc9ef186786b4
mailenosorio <101746912+mailenosorio@users.noreply.github.com>
2024-06-27 21:45:37 -0300

    Revert "add preference: equipo"

--- preferencias.php
+++ preferencias.php
@@ -3,10 +3,8 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-$equipo=(isset($_GET['equipo'])?$_GET['equipo']:"No cargaste tu equipo de fútbol");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
-echo "Tu equipo de fútbol es: $equipo";
 
 ?>
