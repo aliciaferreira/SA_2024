b6a6f20adf790206db6a91a45be4f41f99264538
Alicia Ferreira <alicia@easytechgreen.com>
2024-06-27 21:01:28 -0300

    fix result field music

--- preferencias.php
+++ preferencias.php
@@ -4,7 +4,7 @@ require("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $flor=(isset($_GET['flor'])?$_GET['flor']:"No cargaste una flor preferida");
-$music=(isset($_GET['music'])?$_GET['color']:"No cargaste un tipo de musica preferida");
+$music=(isset($_GET['music'])?$_GET['music']:"No cargaste un tipo de musica preferida");
 $boardgame=(isset($_GET['boardgame'])?$_GET['boardgame']:"No cargaste tu juego de mesa preferido");
 
 echo "Hola $usuario";
