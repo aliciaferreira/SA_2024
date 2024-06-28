6ec7fe17e5bd8ea843992d2e7bd82fda2dcb05eb
libertoria <116813666+libertoria@users.noreply.github.com>
2024-06-27 21:41:32 -0300

    victoria

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,10 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$comida=(isset($_GET['comida'])?$_GET['comida']:"No cargaste tu comida favorita");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Tu comida Favorita es: $comida";
 
 ?>
