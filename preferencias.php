76658f9e8b51b46aeb16706eb65e112a1c2f56c5
Sol Larrea <larreasol97@gmail.com>
2024-06-27 21:30:08 -0300

    agregue cambios

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,12 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$animal=(isset($_GET['animal'])?$_GET['animal']:"No cargaste una animal preferido");
+
 
 echo "Hola $usuario";
+
 echo "Tu color preferido es: $color";
 
+echo "Tu animal preferido es: $animal";
 ?>
