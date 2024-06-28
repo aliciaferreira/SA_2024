50eec0caa24c1c6f497f7d34f72c52828719ee99
Matias Rucci <mn.rucci@gmail.com>
2024-06-27 21:30:11 -0300

    add new features

--- preferencias.php
+++ preferencias.php
@@ -3,8 +3,9 @@ require("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-
+$elementoDeLaTablaPeriodica=(isset($_GET['elementoDeLaTablaPeriodica'])?$_GET['elementoDeLaTablaPeriodica']:"No cargaste un elemento de la tabla periódica preferido");
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Tu elemento de la tabla periódica preferido es: $elementoDeLaTablaPeriodica";
 
 ?>
