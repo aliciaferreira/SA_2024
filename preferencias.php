d904e18c627c73a0eea6e0503f18e7126502c05b
Bogarin1978 <bogarinjorgeramon7@gmail.com>
2024-07-04 14:57:40 -0300

    resolve

--- preferencias.php
+++ preferencias.php
@@ -5,14 +5,16 @@ $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usu
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $equipo=(isset($_GET['equipo'])?$_GET['equipo']:"No cargaste tu equipo de fútbol");
 $deporte=(isset($_GET['deporte'])?$_GET['deprote']:"deporte favoritol");
-$deporte=(isset($_GET['celular'])?$_GET['celular']:"deporte favoritol")
-$dire=(isset($_GET['direccion'])?$_GET['direccion']:"direccion")
+$tango=(isset($_GET['tango'])?$_GET['tango']:"tango");
+$baile=(isset($_GET['baile'])?$_GET['baile']:"baile");
+$licol=(isset($_GET['lineacol'])?$_GET['lineacol']:"linea de colectivos");
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu equipo de fútbol es: $equipo";
 echo "Tu deporte favorito :"$deporte;
-echo"Tu numero de celular ;"$celular;
-echo"Tu direccion "$dire;
+echo"tango ;"$tango;
+echo"Tu baile preferido "$baile;
+echo"Tu linea de colectivos"$licol;
 
 
 ?>
