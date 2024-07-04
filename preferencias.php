3d67c006d44845eb5ee4968af9defd287d73b074
Bogarin1978 <bogarinjorgeramon7@gmail.com>
2024-07-04 14:32:20 -0300

    first commit

--- preferencias.php
+++ preferencias.php
@@ -4,9 +4,13 @@ require("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $equipo=(isset($_GET['equipo'])?$_GET['equipo']:"No cargaste tu equipo de fútbol");
+$deporte=(isset($_GET['deporte'])?$_GET['deprote']:"deporte favoritol");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu equipo de fútbol es: $equipo";
+echo "Tu deporte favorito :"$deporte;
+echo"Tu numero de celular ;"$celular;
+
 
 ?>
