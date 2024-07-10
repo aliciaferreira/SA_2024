b22204d273a37dda8e78a41f70b052525068cc96
MarFerGit <marinafernandarodriguez443@gmail.com>
2024-07-10 20:24:32 -0300

    add preference

--- preferencias.php
+++ preferencias.php
@@ -8,24 +8,26 @@ $flor=(isset($_GET['flor'])?$_GET['flor']:"No cargaste una flor preferida");
 $music=(isset($_GET['music'])?$_GET['color']:"No cargaste un tipo de musica preferida");
 $boardgame=(isset($_GET['boardgame'])?$_GET['boardgame']:"No cargaste tu juego de mesa preferido");
 $animal=(isset($_GET['animal'])?$_GET['animal']:"No cargaste una animal preferido");
-
-
 $artista=(isset($_GET['artista'])?$_GET['artista']:"No cargaste un artista preferido");
-echo "Hola $usuario";
 $clima=(isset($_GET['clima'])?$_GET['clima']:"No cargaste un clima");
-?><br><br><?php
+$comida=(isset($_GET['comida'])?$_GET['comida']:"No cargaste tu comida preferida");
+$letra=(isset($_GET['letra'])?$_GET['letra']:"No cargaste un Estilo de letra");
+
 echo "Hola $usuario";?><br><br><?php
 echo "Tu color preferido es: $color";?><br><br><?php
-echo "Tu clima preferido es: $clima";?><br><br><?php
-echo "Saludos .";
-
-echo "Tu color preferido es: $color";
+echo "Tu serie preferida es: $serie";
 echo "Tu flor preferida es: $flor";
 echo "Tu musica preferida es: $music";
 echo "Tu juego de mesa preferido es: $boardgame";
-
-echo "Tu artista preferido es: $artista";
-
-
 echo "Tu animal preferido es: $animal";
+echo "Tu artista preferido es: $artista";
+echo "Tu clima preferido es: $clima";?><br><br><?php
+echo "Tu comida preferida es: $comida";?><br><br><?php
+echo "Tu Estilo de Letra preferido es: $letra";?><br><br><?php
+
+echo "Saludos $usuario";
+
+
+
+
 ?>
